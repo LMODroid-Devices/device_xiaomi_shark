595817930f7868702e8f98749e18b0f317ca5de3
LMO GerritBot <gerrit@libremobileos.com>
2024-08-09 05:26:42 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_shark.mk
+    $(LOCAL_DIR)/lmodroid_shark.mk
--- lmodroid_shark.mk
+++ lmodroid_shark.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/shark/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_shark
+PRODUCT_NAME := lmodroid_shark
 PRODUCT_DEVICE := shark
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Black Shark
